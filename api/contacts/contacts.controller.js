479e9ee4d3d1d36f64dc7934fb1f1e93f1e47b83
EmilioRosso <7zen@ukr.net>
2020-04-26 17:37:45 +0800

    working

--- api/contacts/contacts.controller.js
+++ api/contacts/contacts.controller.js
@@ -1,33 +1,37 @@
-const fs = require("fs");
-const path = require("path");
+const contactsModel = require("./contacts.models");
 const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
 
-const { promises: fsPromises } = fs;
-
-const contactsPath = path.resolve("db/contacts.json");
-
-async function getUsersList(req, res, next) {
+async function getAllContacts(req, res, next) {
   try {
-    const contacts = await fsPromises.readFile(contactsPath, "utf-8");
-    res.send(contacts);
+    const contacts = await contactsModel.find();
+    res.status(200).json(contacts);
   } catch (error) {
-    next(error);
+    const newError = new CustomError(error.message);
+    return res.status(404).send(newError.message);
   }
+}
+
+function validateGetContactById(req, res, next) {
+  const getContactByIdRules = Joi.objectId(req.params.contactId);
+  const validationResult = Joi.validate(
+    req.params.contactId,
+    getContactByIdRules
+  );
+  if (validationResult.error) {
+    const newError = new CustomError(validationResult.error.details[0].message);
+    return res.status(400).send(newError.message);
+  }
+  next();
 }
 
 async function getContactById(req, res, next) {
   try {
-    const contacts = await fsPromises.readFile(contactsPath, "utf-8");
-    const parsedContacts = JSON.parse(contacts);
-    const contact = parsedContacts.filter(
-      (elem) => elem.id === Number(req.params.contactId)
-    );
-    if (contact.length === 0) {
-      res.status(404).send("No such contact");
-    }
-    res.send(contact[0]);
+    const contact = await contactsModel.findById(req.params.contactId);
+    res.status(200).json(contact);
   } catch (error) {
-    next(error);
+    const newError = new CustomError(error.message);
+    return res.status(404).send(newError.message);
   }
 }
 
@@ -38,84 +42,71 @@ function validateCreateContact(req, res, next) {
     phone: Joi.string().required(),
   });
   const validationResult = Joi.validate(req.body, createContactRules);
-
   if (validationResult.error) {
-    next(error.message);
+    const newError = new CustomError(validationResult.error.details[0].message);
+    return res.status(400).send(newError.message);
   }
   next();
 }
 
 async function createContact(req, res, next) {
   try {
-    const contacts = await fsPromises.readFile(contactsPath, "utf-8");
-    const parsedContacts = JSON.parse(contacts);
-    const newContact = {
-      ...req.body,
-      id: Math.floor(Math.random() * 100 + 10),
-    };
-    parsedContacts.push(newContact);
-    console.log(parsedContacts);
-    await fsPromises.writeFile(contactsPath, JSON.stringify(parsedContacts));
-    res.status(201).send(newContact);
+    const newContact = await contactsModel.create(req.body);
+    res.status(201).json(newContact);
   } catch (error) {
-    next(error);
-  }
-}
-
-async function deleteContact(req, res, next) {
-  try {
-    const contacts = await fsPromises.readFile(contactsPath, "utf-8");
-    const parsedContacts = JSON.parse(contacts);
-    const index = parsedContacts.findIndex(
-      (elem) => elem.id === Number(req.params.contactId)
-    );
-    if (index === -1) {
-      next("No such contact");
-    }
-    parsedContacts.splice(index, 1);
-    await fsPromises.writeFile(contactsPath, JSON.stringify(parsedContacts));
-    res.status(201).send("Deleted sucsessfully");
-  } catch (error) {
-    next(error);
+    const newError = new CustomError(error.message);
+    return res.status(400).send(newError.message);
   }
 }
 
 function validateUpdateContact(req, res, next) {
-  const updateContactRules = Joi.object({
-    name: Joi.string(),
-    email: Joi.string(),
-    phone: Joi.string(),
-  });
-  const validationResult = Joi.validate(req.body, updateContactRules);
+  const getContactByIdRules = Joi.objectId(req.params.contactId);
+  const validationResult = Joi.validate(
+    req.params.contactId,
+    getContactByIdRules
+  );
   if (validationResult.error) {
-    next(error.message);
+    const newError = new CustomError(validationResult.error.details[0].message);
+    return res.status(400).send(newError.message);
   }
   next();
 }
 
 async function updateContact(req, res, next) {
   try {
-    const contacts = await fsPromises.readFile(contactsPath, "utf-8");
-    const parsedContacts = JSON.parse(contacts);
-    const index = parsedContacts.findIndex(
-      (elem) => elem.id === Number(req.params.contactId)
+    const updatedContact = await contactsModel.findByIdAndUpdate(
+      req.params.contactId,
+      { $set: req.body },
+      { new: true }
     );
-
-    if (index === -1) {
-      res.status(404).send("No such contact");
-    }
-    parsedContacts[index] = { ...parsedContacts[index], ...req.body };
-
-    await fsPromises.writeFile(contactsPath, JSON.stringify(parsedContacts));
-    res.status(201).send("Updated successfully");
+    res.status(201).json(updatedContact);
   } catch (error) {
-    next(error);
+    const newError = new CustomError(error.message);
+    return res.status(400).send(newError.message);
   }
 }
 
-function handleError(req, res, next) {
-  const newError = new CustomError(req);
-  return res.status(404).send(newError);
+function validateDeleteContactById(req, res, next) {
+  const getContactByIdRules = Joi.objectId(req.params.contactId);
+  const validationResult = Joi.validate(
+    req.params.contactId,
+    getContactByIdRules
+  );
+  if (validationResult.error) {
+    const newError = new CustomError(validationResult.error.details[0].message);
+    return res.status(400).send(newError.message);
+  }
+  next();
+}
+
+async function deleteContactById(req, res, next) {
+  try {
+    const contact = await contactsModel.findByIdAndDelete(req.params.contactId);
+    res.status(200).json(contact);
+  } catch (error) {
+    const newError = new CustomError(error.message);
+    return res.status(404).send(newError.message);
+  }
 }
 
 class CustomError extends Error {
@@ -126,12 +117,13 @@ class CustomError extends Error {
 }
 
 module.exports = {
-  getUsersList,
-  getContactById,
+  getAllContacts,
   validateCreateContact,
   createContact,
-  deleteContact,
+  validateGetContactById,
+  getContactById,
   validateUpdateContact,
   updateContact,
-  handleError,
+  validateDeleteContactById,
+  deleteContactById,
 };
--- api/contacts/contacts.router.js
+++ api/contacts/contacts.router.js
@@ -1,38 +1,32 @@
 const { Router } = require("express");
-const contactsController = require("./contacts.controller");
+const contactsController = require("./contacts.controllers");
 
 const contactsRouter = Router();
 
-contactsRouter.get(
-  "/",
-  contactsController.getUsersList,
-  contactsController.handleError
-);
+contactsRouter.get("/", contactsController.getAllContacts);
 
 contactsRouter.get(
   "/:contactId",
-  contactsController.getContactById,
-  contactsController.handleError
+  contactsController.validateGetContactById,
+  contactsController.getContactById
 );
 
 contactsRouter.post(
   "/",
   contactsController.validateCreateContact,
-  contactsController.createContact,
-  contactsController.handleError
+  contactsController.createContact
 );
 
 contactsRouter.delete(
   "/:contactId",
-  contactsController.deleteContact,
-  contactsController.handleError
+  contactsController.validateDeleteContactById,
+  contactsController.deleteContactById
 );
 
 contactsRouter.patch(
   "/:contactId",
   contactsController.validateUpdateContact,
-  contactsController.updateContact,
-  contactsController.handleError
+  contactsController.updateContact
 );
 
 module.exports = contactsRouter;
